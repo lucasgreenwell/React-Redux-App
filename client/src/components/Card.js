3acf5ff34598ad15471df35ff75273a739a87eb5
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-01-23 15:28:19 -0600

    styled app

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,25 +1,33 @@
 import React from "react";
 import { connect } from "react-redux";
-import Loader  from "react-loader-spinner";
+import Loader from "react-loader-spinner";
 import { fetchDog } from "../actions/actions";
+import styled from "styled-components";
+
+const PrettyMuchWholeApp = styled.div`
+  height: 75vh;
+  width: 90%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledDog = styled.img`
+  width: 30%;
+  
+`;
 
 function Card(props) {
-    console.log(props)
+  console.log(props);
   return (
-    <div>
+    <PrettyMuchWholeApp>
       <button onClick={props.fetchDog}>I'm a button</button>
       {/* conditional components under here */}
       {props.isLoading && (
-        <Loader
-          type="Puff"
-          color="#00BFFF"
-          height={100}
-          width={100}
-
-        />
+        <Loader type="Puff" color="#00BFFF" height={100} width={100} />
       )}
-      {!props.isLoading && props.dog&& <img src={props.dog}/>}
-    </div>
+      {!props.isLoading && props.dog && <StyledDog src={props.dog} alt="dog" />}
+    </PrettyMuchWholeApp>
   );
 }
 
@@ -27,4 +35,4 @@ const mapStateToProps = state => {
   return { isLoading: state.isLoading, dog: state.dog, error: state.error };
 };
 
-export default connect(mapStateToProps, {fetchDog})(Card);
+export default connect(mapStateToProps, { fetchDog })(Card);

167aa919e8ecc6c5614acc3722e14d0e65d17c1f
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-01-23 15:17:53 -0600

    mvp

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import {connect} from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
+import Loader  from "react-loader-spinner";
+import { fetchDog } from "../actions/actions";
 
 function Card(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+    console.log(props)
+  return (
+    <div>
+      <button onClick={props.fetchDog}>I'm a button</button>
+      {/* conditional components under here */}
+      {props.isLoading && (
+        <Loader
+          type="Puff"
+          color="#00BFFF"
+          height={100}
+          width={100}
+
+        />
+      )}
+      {!props.isLoading && props.dog&& <img src={props.dog}/>}
+    </div>
+  );
 }
 
-export default Card;
+const mapStateToProps = state => {
+  return { isLoading: state.isLoading, dog: state.dog, error: state.error };
+};
+
+export default connect(mapStateToProps, {fetchDog})(Card);
---
+++ client/src/reducers/reducers.js
@@ -0,0 +1,35 @@
+import {
+  FETCHING_DOG_START,
+  FETCHING_DOG_SUCCESS,
+  FETCHING_DOG_FAILURE
+} from "../actions/actions";
+
+const initialState = {
+  isLoading: false,
+  dog: null,
+  error: ''
+};
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type){
+        case FETCHING_DOG_START:
+        return {
+            ...state,
+            isLoading: true
+        }
+        case FETCHING_DOG_SUCCESS:
+        return {
+            ...state, 
+            isLoading: false,
+            dog: action.payload.message
+        }
+        case FETCHING_DOG_FAILURE:
+        return {
+            ...state, 
+            isLoading: false,
+            dog: action.payload
+        }
+        default: 
+            return state;
+    }
+}
